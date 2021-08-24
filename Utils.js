916e805392661a468c98ed20f5990ea0c88932e3
Conni Bug <connorbilham9@googlemail.com>
2021-08-24 03:37:59 +0100

    Remove unneeded imports

--- Utils.js
+++ Utils.js
@@ -1,7 +1,4 @@
 const log = require('@connibug/js-logging');
-var packageInfo = require("./package.json");
-var serverVersion = packageInfo.version;
-const os = require('os');
 
 module.exports.HasChanged = (oldDat, newDat) => {
     if(oldDat.ref != newDat.ref) {
@@ -31,7 +28,6 @@ module.exports.HasChanged = (oldDat, newDat) => {
 const { exec } = require("child_process");
 let needStashREGEX = new RegExp('Please.commit.your.changes');
 let alreadUpToDateREGEX = new RegExp('Already.up.to.date');
-const notificationService = require("./notificationHandler");
 
 module.exports.handleCD = (shouldRestart = false) => {
     console.log("CD");
@@ -40,11 +36,9 @@ module.exports.handleCD = (shouldRestart = false) => {
             if(error.message.match(needStashREGEX)) {
                 exec("git stash", (error, stdout, stderr) => {
                     // As we stashed restart
-                    notificationService.sendMsg("Server Stopped!", "Version: " + serverVersion + "\n" + `Hostname: ${os.hostname()}`);
                     process.exit(1);
                 });
             }
-            notificationService.sendMsg("Server Stopped!", "Version: " + serverVersion + "\n" + `Hostname: ${os.hostname()}`);
             log.error(`error: ${error.message}`);
             
             // As there was an issue restart
@@ -59,7 +53,6 @@ module.exports.handleCD = (shouldRestart = false) => {
         }
         else {
             log.log(`stdout: ${stdout}`);
-            notificationService.sendMsg("Server Stopped!", "Version: " + serverVersion + "\n" + `Hostname: ${os.hostname()}`);
             process.exit(1);
         }
     });
